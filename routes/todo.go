2dbe4e4a18a1cba0229d47b62efcefacf1665743
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-09-23 19:16:03 +0200

    Add routes

--- routes/todo.go
+++ routes/todo.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/AJAkimana/todo-gin/handlers"
+	"github.com/gin-gonic/gin"
+)
+
+func RigisterRoutes(r *gin.Engine) {
+	todo := r.Group("/todos")
+	{
+		todo.GET("", handlers.GetTodo)
+		todo.POST("", handlers.AddTodo)
+		todo.DELETE("/:id", handlers.DeleteTodo)
+	}
+}
