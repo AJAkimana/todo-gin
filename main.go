8553049f0d11cd60361ec3c7176ce3d2d62809d9
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-09-25 17:23:15 +0200

    Add health check

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/AJAkimana/todo-gin/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	routes.RigisterRoutes(r)
-	r.Run(":8000")
+	router := gin.Default()
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	routes.RigisterRoutes(router)
+	router.Run(":8000")
 }
